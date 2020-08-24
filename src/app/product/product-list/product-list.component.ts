e07789ac732d469d9e2f099f12c7164932705b25
m5e <m-sju-ke13@softbank.ne.jp>
2020-08-25 02:52:24 +0900

    途中経過

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -15,6 +16,24 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.productService.getProducts()
-  }
 
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(x) { console.log('次のデータが出力されました：' + x); },
+      error(err) { console.error('エラーが発生しました： ' + err); },
+      complete() { console.log('完了しました'); }
+    });
+
+    console.log('subscribeから抜けました');
+    }
 }
